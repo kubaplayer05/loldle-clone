80115434a37ba0a9a90f40ef5555140faa24440d
Jakub Grzybek <jakubgrzybekinf@gmail.com>
2023-12-20 20:31:39 +0100

    feat: add pwa and styles for mobile devices

---
+++ frontend/public/serviceWorker.js
@@ -0,0 +1,17 @@
+self.addEventListener('install', function(event) {
+    // Perform install steps
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                    // Cache hit - return response
+                    if (response) {
+                        return response;
+                    }
+                    return fetch(event.request);
+                }
+            )
+    );
+});
